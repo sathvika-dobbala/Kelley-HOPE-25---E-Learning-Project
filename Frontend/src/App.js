428057af5067ffa5a6fa7b946447a7f34cbdfafd
FinnRosewell <finnrosewell@gmail.com>
2025-06-24 16:41:36 -0400

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import SimpleChatUI from './SimpleChatUI';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleChatUI />
+    </div>
+  );
+}
+
+export default App;
+
